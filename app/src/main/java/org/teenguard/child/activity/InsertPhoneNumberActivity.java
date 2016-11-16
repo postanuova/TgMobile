3f6609092cb81d5fcf2c2a8702d7d045b1cc987a
postanuova <postanuova@gmail.com>
2016-11-16 20:31:52 +0000

    working on insert phone number

--- app/src/main/java/org/teenguard/child/activity/InsertPhoneNumberActivity.java
+++ app/src/main/java/org/teenguard/child/activity/InsertPhoneNumberActivity.java
@@ -6,10 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import org.teenguard.child.R;
 
 public class InsertPhoneNumberActivity extends AppCompatActivity {
-
+//libphone number
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_phone_number);
+        //set title
+        setTitle(R.string.title_phone_number);
+        //enable back button
+        //getActionBar().setHomeButtonEnabled(true);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        //
     }
 }
