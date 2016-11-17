d24f67177a369e8a3c4ecb771b8942314639366c
postanuova <postanuova@gmail.com>
2016-11-17 17:17:20 +0000

    adding country code picker

--- app/src/main/java/org/teenguard/child/activity/InsertPhoneNumberActivity.java
+++ app/src/main/java/org/teenguard/child/activity/InsertPhoneNumberActivity.java
@@ -2,11 +2,29 @@ package org.teenguard.child.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+import com.mukesh.countrypicker.fragments.CountryPicker;
+import com.mukesh.countrypicker.interfaces.CountryPickerListener;
 
 import org.teenguard.child.R;
 
 public class InsertPhoneNumberActivity extends AppCompatActivity {
-//libphone number
+    //libphone number
+    TextView tvIsValidPhone;
+    EditText edtPhone;
+    EditText edtCountryCode;
+    Button btnValidate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +34,79 @@ public class InsertPhoneNumberActivity extends AppCompatActivity {
         //enable back button
         //getActionBar().setHomeButtonEnabled(true);
         //getActionBar().setDisplayHomeAsUpEnabled(true);
-        //
+        tvIsValidPhone = (TextView) findViewById(R.id.tvIsValidPhone);
+        edtCountryCode = (EditText) findViewById(R.id.edtCountryCode);
+        edtPhone = (EditText) findViewById(R.id.edtPhoneNumber);
+        btnValidate = (Button) findViewById(R.id.btnValidate);
+
+        //country code
+        edtCountryCode.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("parentImageView clicked");
+                CountryPicker picker = CountryPicker.newInstance("Select Country");
+                picker.show(getSupportFragmentManager(), "COUNTRY_PICKER");
+                picker.setListener(new CountryPickerListener() {
+                    @Override
+                    public void onSelectCountry(String name, String code, String dialCode, int flagDrawableResID) {
+                        System.out.println("name = " + name);
+                        System.out.println("code = " + code);
+                        System.out.println("dialCode = " + dialCode);
+                    }
+                });
+            }
+        });
+        //validation button
+        btnValidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String countryCode = edtCountryCode.getText().toString().trim();
+                String phoneNumber = edtPhone.getText().toString().trim();
+                if (countryCode.length() > 0 && phoneNumber.length() > 0) {
+                    if (isValidPhoneNumber(phoneNumber)) {
+                        boolean status = validateUsing_libphonenumber(countryCode, phoneNumber);
+                        if (status) {
+                            tvIsValidPhone.setText("Valid Phone Number (libphonenumber)");
+                        } else {
+                            tvIsValidPhone.setText("Invalid Phone Number (libphonenumber)");
+                        }
+                    } else {
+                        tvIsValidPhone.setText("Invalid Phone Number (isValidPhoneNumber)");
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Country Code and Phone Number is required", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    private boolean isValidPhoneNumber(CharSequence phoneNumber) {
+        if (!TextUtils.isEmpty(phoneNumber)) {
+            return Patterns.PHONE.matcher(phoneNumber).matches();
+        }
+        return false;
+    }
+
+    private boolean validateUsing_libphonenumber(String countryCode, String phNumber) {
+        PhoneNumberUtil phoneNumberUtil = PhoneNumberUtil.getInstance();
+        String isoCode = phoneNumberUtil.getRegionCodeForCountryCode(Integer.parseInt(countryCode));
+        Phonenumber.PhoneNumber phoneNumber = null;
+        try {
+            //phoneNumber = phoneNumberUtil.parse(phNumber, "IN");  //if you want to pass region code
+            phoneNumber = phoneNumberUtil.parse(phNumber, isoCode);
+        } catch (NumberParseException e) {
+            System.err.println(e);
+        }
+
+        boolean isValid = phoneNumberUtil.isValidNumber(phoneNumber);
+        if (isValid) {
+            String internationalFormat = phoneNumberUtil.format(phoneNumber, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
+            Toast.makeText(this, "Phone Number is Valid " + internationalFormat, Toast.LENGTH_LONG).show();
+            return true;
+        } else {
+            Toast.makeText(this, "Phone Number is Invalid " + phoneNumber, Toast.LENGTH_LONG).show();
+            return false;
+        }
     }
 }
